d07560900db578dfd5d61751b1e1d9d8aba53935
Harry <59019655+harry-uxd@users.noreply.github.com>
2025-01-31 16:12:16 +0000

    tidied and documented

--- app/filters.js
+++ app/filters.js
@@ -9,11 +9,13 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+// This filter takes a date string in ISO format and returns it in the GDS format
 addFilter('longDate', (dateString) => {
     const date = DateTime.fromISO(dateString);
     return date.toFormat('d MMMM yyyy');
 });
 
+// This filter takes a JSON string that contains user data and returns the value of the key provided
 addFilter('return', (jsonString, key) => {
     try {
         const json = JSON.parse(jsonString);
